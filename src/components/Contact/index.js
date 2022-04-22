f578525d6076f3fae77ae0793c803fcbe2ccb933
Matt Kolbach <mattkolbach@yahoo.com>
2022-04-22 14:21:54 -0500

    styled contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -33,33 +33,40 @@ function ContactForm() {
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
+        //console.log(formState);
+        //document.querySelectorAll('.form-control').forEach((input) => input.value="")
     }
     //console.log(formState);
 
     //JSX
     return (
-        <section>
-            <h1>Contact me</h1>
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='name'>Name:</label>
-                    <input type='text' name="name" defaultValue={name} onBlur={handleChange} />
+        <section className='container-sm'>
+            <div>
+                <div class="d-flex justify-content-center">
+                    <h1>Contact me</h1>
                 </div>
-                <div>
-                    <label htmlFor='email'>Email address:</label>
-                    <input type='email' name="email" defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor='message'>Message:</label>
-                    <textarea name='message' rows="5" defaultValue={message} onBlur={handleChange} />
-                </div>
-                {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-                )}
-                <button type="submit">Submit</button>
-            </form>
+            
+                <form id="contact-form" onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor='name'>Name:</label>
+                        <input className="d-flex justify-content-end" type='text' name="name" defaultValue={name} onBlur={handleChange} />
+                    </div>
+                    <div>
+                        <label htmlFor='email'>Email address:</label>
+                        <input className="d-flex justify-content-end" type='email' name="email" defaultValue={email} onBlur={handleChange} />
+                    </div>
+                    <div>
+                        <label htmlFor='message'>Message:</label>
+                        <textarea className="d-flex justify-content-end" name='message' rows="5" defaultValue={message} onBlur={handleChange} />
+                    </div>
+                    {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                    )}
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </section>
     );
 }
